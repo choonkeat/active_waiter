f29b70459ae27d6bb1859212e4036f54117a4892
Winston <winston.yongwei@gmail.com>
2015-05-25 17:51:18 +0800

    Layout configured by configuration. Helper modules included if set.

--- app/controllers/active_waiter/application_controller.rb
+++ app/controllers/active_waiter/application_controller.rb
@@ -1,6 +1,17 @@
 module ActiveWaiter
   class ApplicationController < ActionController::Base
-    layout "layouts/application"
-    helper Rails.application.routes.url_helpers
+    layout :active_waiter_layout
+
+    before_action :include_active_waiter_helper
+
+    private
+
+      def active_waiter_layout
+        ActiveWaiter.configuration.layout
+      end
+
+      def include_active_waiter_helper
+        _helpers.module_eval { include ActiveWaiter.configuration.helper } if ActiveWaiter.configuration.helper
+      end
   end
 end
