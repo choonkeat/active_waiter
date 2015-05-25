fd35e1c6f334ef4afe265fdf9c3d62f98a7c0117
Winston <winston.yongwei@gmail.com>
2015-05-25 12:28:03 +0800

    Include standard application layout and route helpers from main rails app.
    - route helpers are included because layouts usually contains navigation links.
    - removed engine's own layout

--- test/controllers/active_waiter/jobs_controller_test.rb
+++ test/controllers/active_waiter/jobs_controller_test.rb
@@ -22,29 +22,39 @@ class ActiveWaiter::JobsControllerTest < ActionDispatch::IntegrationTest
 
   def test_show_non_existent
     assert_raises ActionController::RoutingError do
-      get '/active_waiter', id: "nosuchjob"
+      do_request   id: "nosuchjob"
     end
   end
 
   def test_show_started
     ActiveWaiter.stub :next_uuid, uid do
       assert_equal uid, ActiveWaiter.enqueue(RedirectJob)
-      get '/active_waiter', id: uid
+      do_request id: uid
       assert_equal 200, status
       assert_match "Please wait", document_root_element.to_s
     end
   end
 
+  def test_renders_application_layout
+    ActiveWaiter.stub :next_uuid, uid do
+      assert_equal uid, ActiveWaiter.enqueue(RedirectJob)
+      do_request id: uid
+      assert_equal 200, status
+      assert_template :show
+      assert_template layout: "layouts/application"
+    end
+  end
+
   def test_show_error
     ActiveWaiter.write(uid, error: "oops")
-    get '/active_waiter', id: uid
+    do_request id: uid
     assert_equal 500, status
     assert_match "oops", document_root_element.to_s
   end
 
   def test_show_progress
     ActiveWaiter.write(uid, percentage: 42)
-    get '/active_waiter', id: uid
+    do_request id: uid
     assert_equal 200, status
     assert_match "42%", document_root_element.to_s
   end
@@ -53,7 +63,7 @@ class ActiveWaiter::JobsControllerTest < ActionDispatch::IntegrationTest
     ActiveWaiter.stub :next_uuid, uid do
       perform_enqueued_jobs do
         assert_equal uid, ActiveWaiter.enqueue(RedirectJob)
-        get '/active_waiter', id: uid, download: 1
+        do_request id: uid, download: 1
         assert_equal 200, status
         link = document_root_element.css("a[href='http://other.com/12345']").first
         assert link, "missing hyperlink to returned value"
@@ -66,7 +76,7 @@ class ActiveWaiter::JobsControllerTest < ActionDispatch::IntegrationTest
     ActiveWaiter.stub :next_uuid, uid do
       perform_enqueued_jobs do
         assert_equal uid, ActiveWaiter.enqueue(RedirectJob)
-        get '/active_waiter', id: uid
+        do_request id: uid
         assert_equal 302, status
         assert_equal "http://other.com/12345", response.location
       end
@@ -74,6 +84,10 @@ class ActiveWaiter::JobsControllerTest < ActionDispatch::IntegrationTest
   end
 
   private
+
+    def do_request(id:, download: nil)
+      get '/active_waiter', id: id, download: download
+    end
 
     def uid
       "hello"
